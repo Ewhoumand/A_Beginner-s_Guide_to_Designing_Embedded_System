b9b96d44c99eeea4db45539f92ffd986e13182ae
Ewhoumand <ngouan@yandex.ru>
2026-04-01 14:44:20 +0000

    first button test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "mbed.h"
+// user boutton to command led 1
 
 int main()
 {
-  DigitalIn B1_USER(BUTTON1);
-  
-  DigitalOut LD1(LED1);
+    DigitalIn B1_USER(BUTTON1);
 
-  while (true) {
-    LD1 = B1_USER;
-  }
-  
+    DigitalOut LD1(LED1);
+
+    while (true) {
+        LD1 = B1_USER;
+    }
 }
